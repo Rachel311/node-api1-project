e998ab9acc9290542fca1afd30516eb54cae4922
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-02 10:34:18 -0600

    working on endpoints

--- api/server.js
+++ api/server.js
@@ -1,11 +1,56 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 
 const server = express();
 
-server.get('/', (req, res) =>{
-    res.status(200);
-    res.send('Hello World!')
+server.use(express.json());
+
+// server.get('/', (req, res) =>{
+//     res.json({ message: 'hello' })
+// });
+
+// GET request to call a user
+server.get('/api/users', (req, res)=> {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The users information could not be retrieved',
+            })
+        })
 })
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// GET request to call a user by ID
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch (err) {
+        res.status(500).json({
+            message: 'The user with the specified ID does not exist',
+    }
+})
+
+// POST request to create a new user
+server.post('/api/users', async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        } else {
+            const newUser = await User.insert(req.body)
+        res.status(201).json(newUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'There was an error while saving the user to the database',
+    }
+})
+
+// 
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const port = 5000;
 // START YOUR SERVER HERE
 
 server.listen(port, () => {
-    console.log('Server running on http:')
+    console.log(`Server running on ${port}`)
 })
