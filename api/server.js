9ecc6b3ab671dba928ff433b2e216e9c9ea2abb9
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-01 20:03:05 -0600

    server started

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) =>{
+    res.status(200);
+    res.send('Hello World!')
+})
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
