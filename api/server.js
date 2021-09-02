1077f143dca754f9ba667b568de56ac6dc0dee6f
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-02 11:36:33 -0600

    completed mvp

--- api/server.js
+++ api/server.js
@@ -27,10 +27,16 @@ server.get('/api/users', (req, res)=> {
 server.get('/api/users/:id', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-        res.json(user)
+        if (!user) {
+            res.status(404).json({
+                message:'The user with the specified ID does not exist'
+            })
+        } else {
+            res.json(user)
+        }
     } catch (err) {
         res.status(500).json({
-            message: 'The user with the specified ID does not exist',
+            message: 'The user information could not be retrieved',
     }
 })
 
@@ -51,6 +57,39 @@ server.post('/api/users', async (req, res) => {
     }
 })
 
-// 
+// PUT request to update existing user
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    try {
+        const updated = await User.update(id, body)
+        if (!updated) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            res.json(updated)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The user information could not be modified',
+    }
+})
+
+// DELETE request to delete existing user
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    User.delete(id)
+    .then(deletedUser => {
+        if (!deletedUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+        })
+    })
+    .catch(err) {
+        res.status(500).json({
+            message: 'The user could not be removed',
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
